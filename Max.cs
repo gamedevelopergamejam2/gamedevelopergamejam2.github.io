a7d68a9f872640defb8d212a266138e4ea801a2d
gamedevelopergamejam2 <66121149+gamedevelopergamejam2@users.noreply.github.com>
2020-05-29 15:51:55 +0300

    Add files via upload

---
+++ Max.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Max : MonoBehaviour
+{
+    public float timeStart;
+    public float timeStart2;
+    public Text text;
+    // Start is called before the first frame update
+    void Start()
+    {
+        timeStart = PlayerPrefs.GetFloat("moghtun");
+        timeStart2 = PlayerPrefs.GetFloat("fie");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(timeStart> timeStart2)
+        {
+            timeStart2 = timeStart;
+           
+            PlayerPrefs.SetFloat("fie", timeStart2);
+        }
+        text.text = timeStart2.ToString("F2");
+    }
+}
